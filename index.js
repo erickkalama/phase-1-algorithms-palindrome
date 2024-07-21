f2e6f558edc0f1e12f47b042730327c7c19cfff4
GHOSTFACE <erickkatana4787@gmail.com>
2024-07-21 22:45:59 +0300

    my completion

--- index.js
+++ index.js
@@ -1,25 +1,46 @@
-function isPalindrome(word) {
-  // Write your algorithm here
+// index.js
+
+// Function to check if a string is a palindrome
+function isPalindrome(s) {
+  let left = 0;
+  let right = s.length - 1;
+
+  while (left < right) {
+      if (s[left] !== s[right]) {
+          return false;
+      }
+      left++;
+      right--;
+  }
+
+  return true;
 }
 
-/* 
-  Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
-*/
-
-// You can run `node index.js` to view these console logs
-if (require.main === module) {
-  // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
-
-  console.log("");
-
-  console.log("Expecting: false");
-  console.log("=>", isPalindrome("robot"));
-}
-
+// Export the function for testing
 module.exports = isPalindrome;
+
+// test/indexTest.js
+
+const isPalindrome = require('../index');
+
+describe('isPalindrome', function() {
+    it('returns true for "abba"', function() {
+        assert.strictEqual(isPalindrome('abba'), true);
+    });
+
+    it('returns true for "racecar"', function() {
+        assert.strictEqual(isPalindrome('racecar'), true);
+    });
+
+    it('returns true for "a"', function() {
+        assert.strictEqual(isPalindrome('a'), true);
+    });
+
+    it('returns false for "robot"', function() {
+        assert.strictEqual(isPalindrome('robot'), false);
+    });
+
+    it('returns false for "ab"', function() {
+        assert.strictEqual(isPalindrome('ab'), false);
+    });
+});
